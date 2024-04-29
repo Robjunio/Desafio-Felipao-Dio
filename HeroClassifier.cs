09e3205aac5640050258196daeba815bc0e4d1f8
Roberto Gomes <83621531+Robjunio@users.noreply.github.com>
2024-04-29 12:56:35 -0400

    Create the hero classifier

---
+++ HeroClassifier.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class HeroClassifier
+{
+    public static void Main(string[] args)
+    {
+        float HeroXp;
+        string heroName;
+        string level = "";
+
+        Console.WriteLine("Enter your hero name:");
+        heroName = Console.ReadLine();
+        
+        Console.WriteLine("Enter your xp:");
+        HeroXp = float.Parse(Console.ReadLine());
+        
+        if (HeroXp > 10000) {
+            level = "Radiante";
+        } 
+        else if (HeroXp > 9000)
+        {
+            level = "Imortal";
+        }
+        else if (HeroXp > 8000)
+        {
+            level = "Ascendente";
+        }
+        else if (HeroXp > 7000)
+        {
+            level = "Platina";
+        }
+        else if (HeroXp > 5000)
+        {
+            level = "Ouro";
+        }
+        else if (HeroXp > 2000)
+        {
+            level = "Prata";
+        }
+        else if (HeroXp > 1000)
+        {
+            level = "Bronze";
+        }
+        else if (HeroXp <= 1000)
+        {
+            level = "Ferro";
+        }
+
+        Console.WriteLine("O Herói de nome " + heroName + " está no nível de " + level);
+    }
+}
